e1da252c59dea0af2a81927c0c1f059045063928
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2023-11-28 12:41:08 +0100

    Added materials and scripts

---
+++ Formation Motion/Assets/Scripts/Slot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public GameObject Target;
+    public int Slots;
+    public GameObject SlotPrefab;
+    
+    // THEORY
+    // position_slot = position_leader + orientation_leader * position_slotToLeader 
+    // orientation_slot = orientation_leader + orientation_slotToLeader
+
+    // Square formation
+    // 2 1 2
+    // 3 2 3
+    // 4 3 4
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+
+    }
+}
